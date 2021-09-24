4c2becd55faa7900c38177af67b4a57c80f5b63d
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-24 17:54:37 +0530

    added error msg

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var billAmount = document.querySelector(".billAmount");
 var cashGiven = document.querySelector(".cashGiven");
 var checkButton = document.querySelector("#checkButton");
+const errorMsg = document.querySelector("#error-msg");
 
-checkButton.addEventListener("click", function validateBill(){
-    console.log("clicked")
+checkButton.addEventListener("click", function validateBill() {
+     errorMsg.style.display = "none";
+    if (billAmount.value > 0) {
+        console.log(billAmount.value)
+        if (cashGiven.value >= billAmount.value) {
+
+        } else {
+            makeErrorMessage("Give more Cash!!!");
+        }
+    } else {
+        makeErrorMessage("Invalid bill amount");
+    }
 });
+
+function makeErrorMessage(msg) {
+    errorMsg.style.display = "block";
+    errorMsg.innerText = msg;
+}

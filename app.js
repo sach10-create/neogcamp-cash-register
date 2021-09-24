f97c880c978b0a70b99443452c67029f24f809b6
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-24 18:57:48 +0530

    developed the logic

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ var billAmount = document.querySelector(".billAmount");
 var cashGiven = document.querySelector(".cashGiven");
 var checkButton = document.querySelector("#checkButton");
 const errorMsg = document.querySelector("#error-msg");
+var notesArray = [2000, 500, 100  ,20 , 10,  5 ,1];
+const noOfNotes = document.querySelectorAll(".notes");
 
 checkButton.addEventListener("click", function validateBill() {
      errorMsg.style.display = "none";
     if (billAmount.value > 0) {
-        console.log(billAmount.value)
-        if (cashGiven.value >= billAmount.value) {
+        // console.log(billAmount.value)
+        if (cashGiven.value > billAmount.value) {
+            var amountToBeReturned = cashGiven.value - billAmount. value;
+            calculateChange(amountToBeReturned);
 
         } else {
             makeErrorMessage("Give more Cash!!!");
@@ -16,6 +20,16 @@ checkButton.addEventListener("click", function validateBill() {
         makeErrorMessage("Invalid bill amount");
     }
 });
+
+function calculateChange(amountToBeReturned){
+    console.log("here");
+    for(var i =0 ; i<notesArray.length ; i++){
+        const numberOfnotes = Math.trunc
+        (amountToBeReturned / notesArray[i]);
+        amountToBeReturned %= notesArray[i];
+        noOfNotes[i].innerText = numberOfnotes;
+    }
+}
 
 function makeErrorMessage(msg) {
     errorMsg.style.display = "block";

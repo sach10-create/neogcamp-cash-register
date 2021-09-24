4359960ed64b1960ee05230922b78596f7630d4a
sach10-create <65531346+sach10-create@users.noreply.github.com>
2021-09-24 17:23:27 +0530

    retrived the values and tested using comsole.log

---
+++ app.js
@@ -0,0 +1,7 @@
+var billAmount = document.querySelector(".billAmount");
+var cashGiven = document.querySelector(".cashGiven");
+var checkButton = document.querySelector("#checkButton");
+
+checkButton.addEventListener("click", function validateBill(){
+    console.log("clicked")
+});
